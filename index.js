5641ec30dc2593cd69d6859332e83609fc5e0fbd
Joshua Baldwin <baldwinjd2@appstate.edu>
2023-04-26 17:32:38 -0400

    Sets up structure to send HTML files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello from Phillip and Friends</h1>');
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(80, () => {
